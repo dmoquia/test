13c76eb56904b9e9eb8644d567b7ce3c10070d8c
dmoquia <dennis.moquia@gmail.com>
2022-09-04 12:44:24 +0800

    Merge branch 'master' of https://github.com/dmoquia/doc-health-app

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ app.use("/api/doctor", require("./routes/doctorsRoute"));
 const port = process.env.PORT || 5000;
 
 // this configuration code that well help us deploy to heroku
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//   });
+// }
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 // end of this configuration code that well help us deploy to heroku
